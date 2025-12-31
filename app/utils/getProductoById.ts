1e16e1ae6efeffa489d92c45488b351fff6ad28e
paula.saenzda <paula.saenzda@gmail.com>
2025-12-31 09:04:57 +0000

    ERROR

--- app/utils/getProductoById.ts
+++ app/utils/getProductoById.ts
@@ -1,6 +1,11 @@
 export async function getProductoById(id: string) {
     try {
-      const res = await fetch(`https://fakestoreapi.com/products/${id}`, { cache: 'no-store' });
+      const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        cache: 'no-store',
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        },
+      });
   
       // Si la respuesta no es correcta, lanzamos un error manejable
       if (!res.ok) {
--- app/utils/getProductos.ts
+++ app/utils/getProductos.ts
@@ -13,6 +13,9 @@ export async function getProductos(): Promise<Producto[]> {
   try {
     const res = await fetch("https://fakestoreapi.com/products", {
       cache: "no-store",
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+      },
     });
 
     if (!res.ok) {
